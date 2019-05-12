f75b94be85a66761059a61fd17f8490f20525c68
Home <jamesdordoy@gmail.com>
2019-05-12 10:20:06 +0100

    Corrections to docs.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
 module.exports = {
-    publicPath: `<laravel-vue-datatable>`,
-    // outputDir: path.resolve(__dirname, "./docs")
+    publicPath: `<vue-datatable>`,
+    outputDir: path.resolve(__dirname, "./docs"),
 }
