66bb99ed99196dd7076735d0b949fa89e57290bb
James Dordoy <jamesdordoy@gmail.com>
2019-07-23 18:43:49 +0100

    Custom filter component.

---
+++ src/components/CustomFilters.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="row mb-3">
+        <div class="col-md-3">
+            <select
+                v-model="tableData.length"
+                class="form-control">
+                <option
+                    :key="index"
+                    :value="records"
+                    v-for="(records, index) in perPage">
+                    {{ records }}
+                </option>
+            </select>
+        </div>
+        <div class="col-md-3 offset-md-6">
+            <input
+                name="name"
+                class="form-control"
+                v-model="tableData.search"
+                placeholder="Search Table">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        getData() {
+            this.$emit("getData");
+        }
+    },
+    props: {
+        tableData: {
+            type: Object,
+            default: () => ({}),
+            required: true,
+        },
+        perPage: {
+            type: Array,
+            default: () => ([]),
+            required: true,
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
