485ab2dfa2bcd23aaf63cf50a0d7329a0e7c6190
James Dordoy <jamesdordoy@gmail.com>
2019-09-17 14:50:39 +0100

    Correcting tests.

--- src/__tests__/bootstrap.js
+++ src/__tests__/bootstrap.js
@@ -1,10 +1,14 @@
 // Attach Vue
 global.Vue = require('vue');
 
+import VueRouter from 'vue-router';
+
 //Attach an alert function to the window as JSDOM dosnt do this but our components makes use of the function
 //https://stackoverflow.com/questions/55088482/jest-not-implemented-window-alert
 window.alert = () => {};
 
 import 'jest-localstorage-mock';
+
+Vue.use(VueRouter);
 
 Vue.component('laravel-pagination', require('laravel-vue-pagination'));
