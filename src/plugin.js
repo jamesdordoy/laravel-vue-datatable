f2a0d2cd3f7c8bcd60fe246e55b63fb093e79562
James Dordoy <jamesdordoy@gmail.com>
2019-09-19 13:58:48 +0100

    Bugfix for overriding table body. Added DataTableCell component to plugin export.

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,10 @@
 import DataTable from "./components/DataTable.vue";
+import DataTableCell from "./components/DataTableCell.vue";
 
 export default {
     install(Vue) {
         Vue.component("data-table", DataTable);
+        Vue.component("data-table-cell", DataTableCell);
         Vue.component("laravel-pagination", require('laravel-vue-pagination'));
     },
 };
