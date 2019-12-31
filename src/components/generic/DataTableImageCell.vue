c13a28bd33b0057619386b595ce3636849f30313
James Dordoy <jamesdordoy@gmail.com>
2019-12-31 19:49:47 +0000

    Image resize.

--- src/components/generic/DataTableImageCell.vue
+++ src/components/generic/DataTableImageCell.vue
@@ -1,5 +1,5 @@
 <template>
-    <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d.jpg?s=60">
+    <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d.jpg?s=40">
 </template>
 
 <script>
